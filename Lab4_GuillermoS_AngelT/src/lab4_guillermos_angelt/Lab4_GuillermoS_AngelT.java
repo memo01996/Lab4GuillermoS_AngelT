530fa479765dd4bcaa0d6387fb3580be7ab89d94
memo01996 <guillermo_asf@live.com>
2018-02-16 14:16:17 -0600

    main

--- Lab4_GuillermoS_AngelT/src/lab4_guillermos_angelt/Lab4_GuillermoS_AngelT.java
+++ Lab4_GuillermoS_AngelT/src/lab4_guillermos_angelt/Lab4_GuillermoS_AngelT.java
@@ -1,9 +1,29 @@
 package lab4_guillermos_angelt;
 
+import java.util.Scanner;
+
 public class Lab4_GuillermoS_AngelT {
 
+    static Scanner sc = new Scanner(System.in);
+
     public static void main(String[] args) {
-        
+        switch (menu()) {
+
+        }
     }
 
+    public static int menu() {
+        System.out.println("Menu");
+        System.out.println("1: Agregar");
+        System.out.println("2: Modificar");
+        System.out.println("3: Eliminar");
+        System.out.println("4: Lanzar Viaje");
+        System.out.println("Ingrese Opcion:");
+        int op = sc.nextInt();
+        return op;
+    }//fin menu
+
+    public static void menu2() {
+
+    }
 }
