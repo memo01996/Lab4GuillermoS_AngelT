a4682e8d4353a3c8cc656890fe0c9550db4567cd
memo01996 <guillermo_asf@live.com>
2018-02-16 17:42:03 -0600

    nodo

--- Lab4_GuillermoS_AngelT/src/lab4_guillermos_angelt/Cola.java
+++ Lab4_GuillermoS_AngelT/src/lab4_guillermos_angelt/Cola.java
@@ -1,8 +1,51 @@
 package lab4_guillermos_angelt;
 
-
-
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Cola {
 
+    LinkedList Cola = new LinkedList();
+    private int size = 0;
+    private Nodo head = null;
+
+    public LinkedList getCola() {
+        return Cola;
+    }
+
+    public void setCola(LinkedList Cola) {
+        this.Cola = Cola;
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public Nodo getHead() {
+        return head;
+    }
+
+    public void setHead(Nodo head) {
+        this.head = head;
+    }
+
+    private void queue() {
+
+    }
+
+    private void Peek() {
+        Cola.peek();
+    }
+
+    public boolean isEmpty() {
+        return (size == 0);
+    }
+
+    private void getsized() {
+        Cola.size();
+    }
 }
--- Lab4_GuillermoS_AngelT/src/lab4_guillermos_angelt/Nodo.java
+++ Lab4_GuillermoS_AngelT/src/lab4_guillermos_angelt/Nodo.java
@@ -1,8 +1,24 @@
-
-
 package lab4_guillermos_angelt;
 
-
 public class Nodo {
+
+    private Object elem;
+    private Nodo Next;
+
+    public Object getElem() {
+        return elem;
+    }
+
+    public void setElem(Object elem) {
+        this.elem = elem;
+    }
+
+    public Nodo getNext() {
+        return Next;
+    }
+
+    public void setNext(Nodo Next) {
+        this.Next = Next;
+    }
 
 }
